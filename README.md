3128e10732988a57bfb132836173ce20c42f693d
Simon Zeng <simon.zeng.68@gmail.com>
2019-01-15 22:30:57 -0500

    Committed main.hs

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # interpoly
-Small CLI Polynomial Interpolation
+Small CLI Polynomial Interpolation Tool
+
+README still in TODO phase. In the meantime, you can run `main.hs` with GHC(i) - 
+the functionality is all there, save for some clean up and extra CLI features.
---
+++ main.hs
@@ -0,0 +1,51 @@
+import Data.List
+
+main :: IO ()
+main = do
+    values <- getLine
+    putStrLn . polyformat . solver . (map (read::String->Double)) . words $ values
+
+-- s1 :: String -> String -> String
+-- s1 a b = if isAnagram then "A" else "N"
+--     where
+--         word      = sort a
+--         stars     = sort b
+--         starCount = length $ filter (=='*') stars
+--         isAnagram = drop starCount word == drop starCount stars
+
+enumerate :: (Num a, Enum a) => [b] -> [(a, b)]
+-- transofmrs [a,b,c] into [(0,a), (1,b), (2,c)]
+enumerate = zip [0..]
+
+is_constant :: (Ord a, Fractional a) => [a] -> Bool
+is_constant [] = True
+is_constant (x:xs) = all ((< 0.001) . abs . (subtract x)) xs
+
+differences :: (Eq t, Num t, Num a) => t -> [a] -> [a]
+-- returns the nth differences between elements of lst
+differences 0 lst = if null lst then [0] else lst
+differences n []  = [0]
+differences n lst = differences (n - 1) $ tail $ zipWith (-) lst $ 0:lst
+
+degree :: (Ord a, Fractional a, Enum p, Num p) => [a] -> p
+-- degree :: (Enum p, Eq a, Num p, Num a) => [a] -> p
+degree []  = 0
+degree lst
+  | is_constant lst = 0
+  | otherwise       = succ . degree $ differences 1 lst
+
+factorial :: Int -> Integer
+factorial = product . flip take [1..]
+
+solver :: (Enum a, Ord a, Fractional a) => [a] -> [a]
+solver lst
+  | deg == 0  = [a]
+  | otherwise = [a] ++ solver [x - a*(n^deg) | (n, x) <- enumerate lst]
+  where
+      deg     = degree lst
+      a       = (head $ differences deg lst) / fromIntegral (factorial deg)
+
+polyformat :: Show a => [a] -> String
+polyformat = (intercalate " + ") . (map poly) . reverse . enumerate . reverse . (map show)
+    where 
+        poly x = if (fst x) /= 0 then snd x ++ "x^" ++ show (fst x) else snd x
