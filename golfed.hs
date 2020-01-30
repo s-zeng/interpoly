8cd6e8162330f54f6d9bd93d65d2ed5180b8dbf2
Simon Zeng <simon.zeng.68@gmail.com>
2020-01-29 18:09:53 -0800

    Update golfed.hs

--- golfed.hs
+++ golfed.hs
@@ -1,16 +1,16 @@
 import Data.List
 main=getLine>>=(putStrLn.f.p.(map(read::String->Double)).words)
 e=zip[0..]
-i(x:y)=all((< 0.1).abs.(subtract x))y
+i(x:y)=all((<0.1).abs.((-)x))y
 d 0 l=l
 d n l=d(n-1)$tail$zipWith(-)l$0:l
 t=True
 g l
  |i l=0
- |t=succ.g$d 1 l
+ |t=(+1).g$d 1 l
 s l
  |g l==0=[a]
- |t=[a]++s[x-a*(n^(g l))|(n,x)<-e l] where a=(head$d(g l)l)/fromIntegral(product[1..g l])
+ |t=[a]++s[x-a*(n^(g l))|(n,x)<-e l] where a=(d(g l)l)!!0/(product[1..g l])
 p l=s l++replicate((g l)+1-length(s l))0
 b=reverse
-f=(intercalate" + ").(map y).b.e.b.(map show) where y x=snd x++"x^"++show(fst x)
+f=(intercalate" + ").(y<$>).b.e.b.(show<$>) where y(a,b)=b++"x^"++show a
